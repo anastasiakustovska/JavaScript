17c3ee77c665ad02ec2cb2b903cac649931b7726
anastasiakustovska <anastasiukraine@gmail.com>
2022-07-19 20:46:59 +0300

    finish homework

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let name = 'Anastasia';
-let surname='Kustovska'
+let surname = 'Kustovska'
 
-alert(name+' '+surname);
+alert(name + ' ' + surname);
 
 let firstName = 'Victoria';
-let lastName='Kustovska'
+let lastName = 'Kustovska'
 
-alert(firstName+' '+lastName);
+alert(firstName + ' ' + lastName);
 
 /*
 let 4irstName = 'Victoria';
@@ -26,19 +26,19 @@ alert("Привіт, " + yourName);
 
 const CURRENT_YEAR = 2022;
 let yourBirthday = prompt("Який рік народження?");
-alert( CURRENT_YEAR - parseInt(yourBirthday));
+alert(CURRENT_YEAR - parseInt(yourBirthday));
 
 let length = prompt("Яка довжина сторони квадрату?");
 alert(parseFloat(length) * 4);
 
 let radius = prompt("Який радіус кола?");
-alert (Math.PI * Math.pow(parseFloat(radius), 2));
+alert(Math.PI * Math.pow(parseFloat(radius), 2));
 
 let distance = prompt("Яка відстань в кілометрах між містами?");
 let time = prompt("За який проміжок часу потрібно дістатись?");
-alert(parseFloat (distance)/ parseFloat (time));
+alert(parseFloat(distance) / parseFloat(time));
 
-const COEFFICIENT= 0.978;
+const COEFFICIENT = 0.978;
 let amount = prompt("Скільки в тебе баксів?");
 
 let result = parseFloat(amount) * COEFFICIENT;
