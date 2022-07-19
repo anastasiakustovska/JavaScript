3c5f0e5733489bf341f39a6a461166d74947fada
anastasiakustovska <anastasiukraine@gmail.com>
2022-07-19 10:17:38 +0300

    init

---
+++ main.js
@@ -0,0 +1,24 @@
+let name = 'Anastasia';
+let surname='Kustovska'
+
+alert(name+' '+surname);
+
+let firstName = 'Victoria';
+let lastName='Kustovska'
+
+alert(firstName+' '+lastName);
+
+/*
+let 4irstName = 'Victoria';
+let __astName='Kustovska'
+
+let 53a = 'Victoria';
+let 6464e='Kustovska'
+*/
+//single line comment
+
+let my_phone_number = 99; //snake_case
+let MyPhoneNumber = 99; //PascalCase
+let myPhoneNumber = 99; //camelCase
+
+
