d87cac4636b982e3dab09ea501ec62c814da7df2
anastasiakustovska <anastasiukraine@gmail.com>
2022-08-15 01:06:18 +0300

    finish tasks

--- 09/main.js
+++ 09/main.js
@@ -29,18 +29,84 @@ class Person {
     }
 }
 
-const people = [new Person(1, 'Jane', 18), new Person(2, 'Barbara', 15), new Person(3, 'Tom', 19), new Person(4, 'John', 92),]
+const people = [new Person(1, 'Jane', 18), new Person(2, 'Barbara', 15), new Person(3, 'Tom', 19), new Person(4, 'John', 92), new Person(5, 'Barnaby', 10),];
 
 const tableBody = document.querySelectorAll('#people tbody')[0];
 
-people.forEach(person => {
-    const row = document.createElement('tr');
+function renderTable(items = []) {
+    items.forEach(person => {
+        const row = document.createElement('tr');
 
-    row.innerHTML = `
+        row.innerHTML = `
         <th scope="row">${person.id}</th>
         <td>${person.name}</td>
         <td>${person.age}</td>
     `;
 
-    tableBody.appendChild(row)
+        tableBody.appendChild(row)
+    })
+}
+
+renderTable(people);
+
+function clearTable() {
+    tableBody.innerHTML = "";
+}
+
+const headers = Array.from(document.getElementsByClassName("table-heading"));
+
+headers.forEach(header => {
+    header.addEventListener("click", (event) => {
+        const column = event.target.getAttribute('data-order');
+
+        const sortedPeople = people.sort((firstPerson, secondPerson) => {
+            if (firstPerson[column] < secondPerson[column]) {
+                return -1;
+            }
+            if (firstPerson[column] > secondPerson[column]) {
+                return 1;
+            }
+            return 0;
+        })
+        clearTable();
+        renderTable(sortedPeople)
+    })
 })
+
+//3
+
+const resizableElement = document.getElementById('resizable');
+const resizer = document.createElement('div');
+let startX, startY, startWidth, startHeight;
+
+resizer.classList.add('resizer');
+resizableElement.appendChild(resizer);
+resizer.addEventListener('mousedown', onResize);
+
+resizableElement.addEventListener('click', function init() {
+    resizableElement.removeEventListener('click', init);
+    resizableElement.className = resizableElement.className + ' resizable';
+});
+
+const resize = (e) => {
+    resizableElement.style.width = (startWidth + e.clientX - startX) + 'px';
+    resizableElement.style.height = (startHeight + e.clientY - startY) + 'px';
+}
+
+const onResizeStop = () => {
+    document.documentElement.removeEventListener('mousemove', resize);
+    document.documentElement.removeEventListener('mouseup', onResizeStop);
+}
+
+function onResize(e) {
+    startX = e.clientX;
+    startY = e.clientY;
+
+    startWidth = parseInt(document.defaultView.getComputedStyle(resizableElement).width, 10);
+    startHeight = parseInt(document.defaultView.getComputedStyle(resizableElement).height, 10);
+
+    console.log({startY, startX, startWidth, startHeight})
+
+    document.documentElement.addEventListener('mousemove', resize);
+    document.documentElement.addEventListener('mouseup', onResizeStop);
+}
