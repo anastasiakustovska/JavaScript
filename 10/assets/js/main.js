f22b2a3cb8227da9fa8bb6cb8dd01270273d5e00
anastasiakustovska <anastasiukraine@gmail.com>
2022-08-16 13:18:46 +0300

    finish jQuery homework

--- 10/assets/js/main.js
+++ 10/assets/js/main.js
@@ -1,1 +1,23 @@
+$(document).ready(function () {
+    $('.headings-item').click(function (){
+        const id = $(this).data('page');
+        $('.content').addClass('hidden');
+        $('#' + id).removeClass('hidden');
+    });
 
+    $('.star').click(function() {
+        const mark = $(this).attr('data-value');
+
+        let counter = 1;
+
+        $('.star').each(function () {
+            if (counter > mark) {
+                return;
+
+            }
+            $(this).addClass('star-gold');
+
+            counter++;
+        });
+    })
+});
