9c9895e44a24e15ea2b724621b21bc538f93e252
anastasiakustovska <anastasiukraine@gmail.com>
2022-08-20 00:13:20 +0300

    start Slick Slider homework

--- 11/assets/js/main.js
+++ 11/assets/js/main.js
@@ -1,14 +1,12 @@
-
-$(function(){
-  $(".top-slider").slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    dotsClass: 'slick-dots top-dots'
-  });
-
-})
+$(document).ready(function () {
+    $(".slider").slick({
+      dots: true,
+      infinite: true,
+      speed: 0,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dotsClass: 'slick-dots top-dots'
+    });
+});
   
