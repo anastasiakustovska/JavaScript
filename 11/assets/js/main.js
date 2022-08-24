94310c4a64653d743649e4ee47cd77d8b70a65d5
anastasiakustovska <anastasiukraine@gmail.com>
2022-08-24 12:54:50 +0300

    add main

--- 11/assets/js/main.js
+++ 11/assets/js/main.js
@@ -1,12 +1,130 @@
 $(document).ready(function () {
-    $(".slider").slick({
-      dots: true,
-      infinite: true,
-      speed: 0,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      dotsClass: 'slick-dots top-dots'
-    });
+    (function () {
+        $(".arrivals__slider").slick({
+            infinite: true,
+            variableWidth: true,
+            arrows: false,
+            dotsClass: 'slick-dots top-dots',
+            accessibility: true,
+            autoplay: false,
+            autoplaySpeed: 800,
+            speed: 1000,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            pauseOnHover: true,
+            responsive: [
+                {
+                    breakpoint: 1000,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows: true,
+                    }
+                },
+                {
+                    breakpoint: 900,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows: true,
+                    }
+                },
+
+                {
+                    breakpoint: 550,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 368,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: true,
+                        dotsClass: 'slick-dots top-dots',
+                    }
+                },
+            ]
+        });
+        $('.arrivals__arrow').click(function() {
+            $('.arrivals__slider').slick($(this).hasClass('arrivals__next') ? 'slickNext' : 'slickPrev');
+        })
+    })();
+
+    (function () {
+        $(".partners__slider").slick({
+            infinite: true,
+            variableWidth: true,
+            arrows: false,
+            dotsClass: 'slick-dots top-dots',
+            accessibility: true,
+            autoplay: false,
+            autoplaySpeed: 800,
+            speed: 1000,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            pauseOnHover: true,
+            responsive: [
+                {
+                    breakpoint: 1000,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows: true,
+                    }
+                },
+                {
+                    breakpoint: 900,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        arrows: true,
+                    }
+                },
+
+                {
+                    breakpoint: 550,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 368,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: true,
+                        dotsClass: 'slick-dots top-dots',
+                    }
+                },
+            ]
+        });
+        $('.partners__arrow').click(function() {
+            $('.partners__slider').slick($(this).hasClass('partners__next') ? 'slickNext' : 'slickPrev');
+        })
+    })();
+
+    (function () {
+        $(".hero--slider").slick({
+            infinite: true,
+            arrows: false,
+            dotsClass: 'slick-dots top-dots dots-hero',
+            accessibility: true,
+            autoplay: false,
+            autoplaySpeed: 800,
+            speed: 1000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            pauseOnHover: true,
+            dots: true,
+        });
+    })();
 });
   
